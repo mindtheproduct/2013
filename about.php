4396529b87b2df09fa45656138549d75d9d2dab5
Martin Eriksson <martin@mindtheproduct.com>
2013-03-20 11:10:04 -0400

    Updated about us

--- about.php
+++ about.php
@@ -8,14 +8,14 @@
     <h1>About</h1>
     <div class="clear">
     <div class="about-content">
-	    <h4>MindTheProduct 2013 is brought to you by a team of four product managers who spend all day building start-ups, and all night building a community for other product managers.</h4>
-	    <p><a href="#">Martin</a> founded <a href="#">ProductTank</a>, a monthly product management meet up in London, which has been running for two years and now has over 900 members in the group and gets 120+ attendees at every meetup.</p>
+	    <h4>MindTheProduct 2013 is brought to you by a team of product managers who spend all day building start-ups, and all night building a community for other product managers.</h4>
+	    <p><a href="http://www.martineriksson.com">Martin</a> founded <a href="http://www.producttank.com">ProductTank</a>, a monthly product management meet up in London, which has been running for three years and now has over 1,600 members in the group and gets 300+ attendees at every meetup.</p>
 	
-	    <p>Janna and Simon co-founded ProductCamp London bi-annually - a collaborative, user organized unconference based on the global ProductCamp.org.</p>
+	    <p><a href="http://www.simplybastow.com">Janna</a> and <a href="https://twitter.com/simoncast">Simon</a> co-founded and run <a href="http://www.productcamplondon.com/">ProductCamp London</a> bi-annually - a collaborative, user organized unconference based on the global ProductCamp.org.</p>
 	
-	    <p>Together with Sam we got together and founded MindtheProduct.com, a blog focused on product management, which we define as the intersection between design, technology and business. The blog combines our own editorial with community contributed posts.</p>
+	    <p>Together we got together and founded <a href="http://www.mindtheproduct.com">Mind the Product</a>, a blog focused on product management, which we define as the intersection between design, technology and business. The blog combines our own editorial with community contributed posts.</p>
 	
-	    <p>We are a not for profit company, funded by our generous sponsors and ticket sales, and driven by a passion for great products and the people who build them.</p>
+	    <p>We are funded by our generous sponsors and ticket sales, and driven by a passion for great products and the people who build them.</p>
 	    
 	    <h6>This site was designed by</h6>
 	    <a href="http://thisiscapra.com"><img src="assets/images/capra-logo.png" alt="Capra" /></a>
