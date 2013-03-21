50bf70be7e0af16180473936ac26e86cecc6037d
Martin Eriksson <martin@mindtheproduct.com>
2013-03-21 14:34:41 -0400

    CDN for background video

--- includes/_video.php
+++ includes/_video.php
@@ -11,6 +11,6 @@
     $(function() {
         var BV = new $.BigVideo();
         BV.init();
-        BV.show('mtp-ambience.mp4',{ambient:true},{doLoop:true});
+        BV.show('http://cdn.mindtheproduct.com/mtp-ambience.mp4',{ambient:true},{doLoop:true});
     });
 </script>
