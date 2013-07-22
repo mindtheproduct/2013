b0e6b06d374e89fb335a5f867105a67d3e1c99d6
Kat Neville <kassy4@gmail.com>
2013-07-22 10:17:54 +0100

    html additions for the schedule before styling begins

--- schedule.php
+++ schedule.php
@@ -11,80 +11,104 @@
 
     <h1>Schedule</h1>
     
-    <!--div class="schedule-summary">
+    <div class="schedule-summary">
         
     	<ul>
-    		<li><a href="#"><time>10:00</time>Martin Eriksson</a></li>
-    		<li><a href="#"><time>11:00</time>Kat Neville</a></li>
-    		<li><time>10:00</time>BREAK</li>
-    		
+            <li><time>09.00</time>Registration</a></li>
+    		<li><time>10.00</time>Intro</a></li>
+    		<li><a href="#sierra"><time>10.15</time>Kathy Sierra</a></li>
+            <li><a href="#berkowski"><time>11.00</time>George Berkowski</a></li>
+    		<li><time>11.30</time>Break</li>
+    		<li><a href="#harford"><time>12.00</time>Tim Harford</a></li>
+            <li><a href="#goto"><time>12.30</time>Kelly Goto</a></li>
+            <li><a href="#lunch"><time>13.00</time>Rackspace Lunch (provided)</a></li>
+            <li><a href="#vlaskovitscooper"><time>14.30</time>Patrick Vlaskovits &amp; Brant Cooper</a></li>
+            <li><a href="#bowles"><time>15.00</time>Cennydd Bowles</a></li>
+            <li><a href="#musa"><time>15.30</time>Aziz Musa</a></li>
+            <li><time>16.00</time>Break</li>
+            <li><a href="#adams"><time>16.30</time>Paul Adams</a></li>
+            <li><a href="#fake"><time>17.00</time>Caterina Fake</a></li>
+            <li><time>17.45</time>Closing remarks</li>
+            <li><a href="#party"><time>18.00</time>After party by eBay</a></li>
     	</ul>
-    </div-->
+    </div>
     <ul class="schedule-details">
-    	
-
-        <li>
-            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/caterina-fake.png" alt="Caterina Fake" />
+    	<li class="non-speaker">
             <div class="content">
-                <!--<time></time>-->
-                <h2>Caterina Fake</h2>
-                <p class="position">Founder, Findery<br />
-                <a href="https://twitter.com/caterina">@caterina</a></p>
-                <p>Caterina is the founder and CEO of Findery but she is best known for co-founding Flickr, almost certainly the best photo sharing and management application out there. She also co-founded Hunch, serves as Chairman of the Board at Etsy, serves on the board of Creative Commons and is an active angel investor.</p>
+                <time>09.00</time>
+                <h3>Registration</h3>
+            </div>
+        </li>
+        <li class="non-speaker">
+            <div class="content">
+                <time>10.00</time>
+                <h3>Intro</h3>
             </div>
         </li>
         <li>
+            <a id="sierra" />
             <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/kathy-sierra.png" alt="Kathy Sierra" />
             <div class="content">
-                <!--<time></time>-->
+                <time>10.15</time>
                 <h2>Kathy Sierra</h2>
-                <p class="position">Blogger &amp; Author<br />
+                <p class="position">Blogger &amp; Author</p>
                 <p>Kathy Sierra has for a long time passionately espoused her insights on how to use “brain friendly” techniques to create passionate users. She designed and taught New Media/Interaction Design courses at UCLA Extension before creating the best-selling Head First series of books for O’Reilly and is the author of a Technorati Top 100 blog, “Creating Passionate Users.”</p>
             </div>
         </li>
-    	<li>
-	    	<img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/cennydd-bowles.png" alt="Cennydd Bowles" />
-	    	<div class="content">
-	    		<!--<time></time>-->
-	    		<h2>Cennydd Bowles</h2>
-	    		<p class="position">Product Designer, Twitter<br />
-	    		<a href="http://twitter.com/cennydd">@cennydd</a></p>
-	    		<p>Digital product designer at Twitter. Over the past decade – including a stint at acclaimed consultancy Clearleft – he has advised dozens of companies on the benefits of customer-focused design. Cennydd is also the co-author of Undercover User Experience Design and writes a monthly column for A List Apart.</p>
-	    	</div>
-    	</li>
         <li>
-            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/tim-harford.png" alt="Tim Harford" />
-            <div class="content">
-                <!--<time></time>-->
-                <h2>Tim Harford</h2>
-                <p class="position">The Undercover Economist<br />
-                <a href="http://twitter.com/timharford">@timharford</a></p>
-                <p>Tim Harford is a senior columnist for the Financial Times. His long-running column, “The Undercover Economist”, reveals the economic ideas behind everyday experiences. Tim’s first book, “The Undercover Economist” has sold one million copies worldwide in almost 30 languages. As a broadcaster, Tim has presented television and radio series for the BBC, most famously “More or Less” on Radio 4. He is an evangelist for the power of economics, wisely used, and has spoken at both TED and PopTech.</p>
-            </div>
-        </li>
-        <li>
-            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/kelly-goto.png" alt="Kelly Goto" />
-            <div class="content">
-                <!--<time></time>-->
-                <h2>Kelly Goto</h2>
-                <p class="position">Founder and Principal, <a href="http://www.gotomedia.com">gotomedia</a><br />
-                <p>As an evangelist for design ethnography, Kelly is dedicated to understanding how people integrate products and services into their daily lives. gotomedia is an industry leader in research-driven, people-friendly interface design and strategy for web, mobile and product solutions for clients including Samsung, Hyundai, and Adobe. Her book, Web Redesign 2.0: Workflow That Works, is a global standard for user-centered design principles.</p>
-            </div>
-        </li>
-        <li>
+            <a id="berkowski" />
             <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/george-berkowski.png" alt="George Berkowski" />
             <div class="content">
-                <!--<time></time>-->
+                <time>11.00</time>
                 <h2>George Berkowski</h2>
                 <p class="position">Chief Product Officer, Hailo<br />
                 <a href="http://twitter.com/georgeberkowski">@georgeberkowski</a></p>
                 <p>George runs product at hit startup Hailo, one of London’s fastest growing startups and is the Chairman of the MIT Enterprise Forum UK. He co-founded and ran product at WooMe, which grew to 10 million users before being acquired by Zoosk, served as the Head of Internet Strategy and eCommerce at BT, and as the VP of Marketing for MirCorp - who made Dennis Tito the first space tourist by arranging his trip to the ISS.</p>
             </div>
         </li>
+        <li class="non-speaker">
+            <div class="content">
+                <time>11.30</time>
+                <h3>Break</h3>
+            </div>
+        </li>
         <li>
+            <a id="harford" />
+            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/tim-harford.png" alt="Tim Harford" />
+            <div class="content">
+                <time>12.00</time>
+                <h2>Tim Harford</h2>
+                <p class="position">The Undercover Economist<br />
+                <a href="http://twitter.com/timharford">@timharford</a></p>
+                <p>Tim Harford is a senior columnist for the Financial Times. His long-running column, “The Undercover Economist”, reveals the economic ideas behind everyday experiences. Tim’s first book, “The Undercover Economist” has sold one million copies worldwide in almost 30 languages. As a broadcaster, Tim has presented television and radio series for the BBC, most famously “More or Less” on Radio 4. He is an evangelist for the power of economics, wisely used, and has spoken at both TED and PopTech.</p>
+            </div>
+        </li>
+
+        <li>
+            <a id="goto" />
+            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/kelly-goto.png" alt="Kelly Goto" />
+            <div class="content">
+                <time>12:30</time>
+                <h2>Kelly Goto</h2>
+                <p class="position">Founder and Principal, <a href="http://www.gotomedia.com">gotomedia</a></p>
+                <p>As an evangelist for design ethnography, Kelly is dedicated to understanding how people integrate products and services into their daily lives. gotomedia is an industry leader in research-driven, people-friendly interface design and strategy for web, mobile and product solutions for clients including Samsung, Hyundai, and Adobe. Her book, Web Redesign 2.0: Workflow That Works, is a global standard for user-centered design principles.</p>
+            </div>
+        </li>
+        <li>
+            <a id="lunch" />
+            <img src="assets/images/speakers/rackspace-sponsors.png" alt="Sponsored by Rackspace" />
+            <div class="content">
+                <time>13.00</time>
+                <h2>Rackspace Lunch (provided)</h2>
+                <p>Workshop 1 description goes here</p>
+                <p>Workshop 2 description goes here</p>
+            </div>
+        </li>
+        <li>
+            <a id="vlaskovitscooper" />
             <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/patrick-brant.png" alt="Patrick Vlaskovits and Brant Cooper" />
             <div class="content">
-                <!--<time></time>-->
+                <time>14.30</time>
                 <h2>Patrick Vlaskovits and Brant Cooper</h2>
                 <p class="position">Authors, The Lean Entrepreneur<br />
                 <a href="http://twitter.com/pv">@pv</a> and <a href="http://twitter.com/brantcooper">@brantcooper</a></p>
@@ -92,9 +116,39 @@
             </div>
         </li>
         <li>
+            <a id="bowles" />
+            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/cennydd-bowles.png" alt="Cennydd Bowles" />
+            <div class="content">
+                <time>15.00</time>
+                <h2>Cennydd Bowles</h2>
+                <p class="position">Product Designer, Twitter<br />
+                <a href="http://twitter.com/cennydd">@cennydd</a></p>
+                <p>Digital product designer at Twitter. Over the past decade – including a stint at acclaimed consultancy Clearleft – he has advised dozens of companies on the benefits of customer-focused design. Cennydd is also the co-author of Undercover User Experience Design and writes a monthly column for A List Apart.</p>
+            </div>
+        </li>
+        <li>
+            <a id="musa" />
+            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/aziz-musa.png" alt="Aziz Musa" />
+            <div class="content">
+                <time>15.30</time>
+                <h2>Aziz Musa</h2>
+                <p class="position">Digital Director, Pegasus<br />
+                <a href="http://twitter.com/azizmusa">@azizmusa</a>
+                </p>
+                <p>Aziz runs the Digital Consulting arm of Pegasus, an award winning specialist agency. Previously of PhotoBox, lastminute.com and RBI, Aziz has held just about every product role that exists from Product Owner to Global Product Strategy Director. Aziz presents at various conferences using psychology, philosophy and empirical study to demonstrate the importance of creating beautiful products whilst driving core KPIs.</p>
+            </div>
+        </li>
+        <li class="non-speaker">
+            <div class="content">
+                <time>16.00</time>
+                <h3>Break</h3>
+            </div>
+        </li>
+        <li>
+            <a id="adams" />
             <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/paul-adams.png" alt="Paul Adams" />
             <div class="content">
-                <!--<time></time>-->
+                <time>16.30</time>
                 <h2>Paul Adams</h2>
                 <p class="position">Head of Product Design, Intercom<br />
                 <a href="http://twitter.com/padday">@padday</a>
@@ -103,16 +157,32 @@
             </div>
         </li>
         <li>
-            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/aziz-musa.png" alt="Aziz Musa" />
+            <a id="fake" />
+            <img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/speakers/caterina-fake.png" alt="Caterina Fake" />
             <div class="content">
-                <!--<time></time>-->
-                <h2>Aziz Musa</h2>
-                <p class="position">Digital Director, Pegasus<br />
-                <a href="http://twitter.com/azizmusa">@azizmusa</a>
-                </p>
-                <p>Aziz runs the Digital Consulting arm of Pegasus, an award winning specialist agency. Previously of PhotoBox, lastminute.com and RBI, Aziz has held just about every product role that exists from Product Owner to Global Product Strategy Director. Aziz presents at various conferences using psychology, philosophy and empirical study to demonstrate the importance of creating beautiful products whilst driving core KPIs.</p>
+                <time>17.00</time>
+                <h2>Caterina Fake</h2>
+                <p class="position">Founder, Findery<br />
+                <a href="https://twitter.com/caterina">@caterina</a></p>
+                <p>Caterina is the founder and CEO of Findery but she is best known for co-founding Flickr, almost certainly the best photo sharing and management application out there. She also co-founded Hunch, serves as Chairman of the Board at Etsy, serves on the board of Creative Commons and is an active angel investor.</p>
             </div>
         </li>
+        <li class="non-speaker">
+            <div class="content">
+                <time>17.45</time>
+                <h3>Closing remarks</h3>
+            </div>
+        </li>
+        <li>
+            <a id="party" />
+            <img src="assets/images/speakers/ebay-sponsors.png" alt="Ebay" />
+            <div class="content">
+                <time>18.00</time>
+                <h2>After Party by ebay</h2>
+                <p>Workshop 1 description goes here</p>
+            </div>
+        </li>
+        
     
     </ul>    
 
