edb052b9f3841328b4ce00febad36510f36b1e7b
Martin Eriksson <martin@mindtheproduct.com>
2013-09-03 08:46:08 -0400

    Adding rackspace and mindcandy

--- index.php
+++ index.php
@@ -19,11 +19,15 @@
     	<h6>Signature sponsor</h6>
       <a href="http://ebay.com"><img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/logos/ebay.png" alt="Ebay" /></a>
       <h6>Sponsors</h6>
+      <a href="http://www.rackspace.com"><img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/logos/rackspace.png" alt="Rackspace" /></a>
+      <br/>
       <a href="http://www.nexmo.com"><img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/logos/nexmo.png" alt="Nexmo" /></a>
       <br/>
       <a href="http://skype.com"><img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/logos/skype.png" alt="Skype" /></a>
       <br/>
       <a href="http://www.eventbrite.co.uk/l/social/?ref=mtp"><img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/logos/eventbrite.png" alt="Eventbrite" /></a>
+      <br/>
+      <a href="http://www.mindcandy.com"><img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/logos/mindcandy.png" alt="MindCandy" /></a>
     </div>
   </div>
 </div><!--closes wrapper-->
