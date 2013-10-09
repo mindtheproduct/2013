d1f95e6867b647bdb38d67fa9397f1acca082296
Kat Neville <kassy4@gmail.com>
2013-10-09 19:11:39 +0100

    it is now over, so let us let people watch videos etc. styling for martin to fill in

--- includes/_nav.php
+++ includes/_nav.php
@@ -6,9 +6,6 @@
 
   <ul>
     <li class="date"><a href="/" <?php if($page == "home") { echo 'class="current"'; } ?>><span>FRI  27 SEPT  2013</span>  IN LONDON</a></li>
-    <li><a href="schedule"  <?php if($page == "schedule") { echo 'class="current"'; } ?>>schedule</a></li>
-    <li><a href="location"  <?php if($page == "location") { echo 'class="current"'; } ?>>location</a></li>
     <li><a href="about" <?php if($page == "about") { echo 'class="current"'; } ?>>about</a></li>
   </ul>
-  <div class="button"><a href="http://mindtheproduct2013.eventbrite.com">Buy Tickets<span>SOLD OUT!</span></a></div>
 </nav>
--- index.php
+++ index.php
@@ -4,17 +4,29 @@
   include 'includes/_header.php'; 
 ?>
 
-  <h1>Building products customers love</h1>
+  <h1>MindTheProduct 2013 is over!</h1>
   <div class="content-home">
-    <h3><span>FRI  27 SEPT  2013</span> IN LONDON</h3>
+    <h3>Watch the videos and relive the magic.</h3>
   
-    <p>Mind the Product 2013 is a one-day, one-track conference that brings together product leaders from around the world to explore the intersection of design, technology and business in order to inspire you to build products your customers love.</p>
+    <div class="columns"
+    <p>We hope you enjoyed this year’s sessions.  Missed it?  You can watch the videos, read reviews and more from the event on MindTheProduct’s blog.</p>
+
+    <ul>
+      <li><a href="#">Kelly Goto</a></li>
+      <li><a href="#">Andy Budd</a></li>
+      <li><a href="#">Kelly Goto</a></li>
+      <li><a href="#">Kelly Goto</a></li>
+      <li><a href="#">Kelly Goto</a></li>
+      <li><a href="#">Kelly Goto</a></li>
+      <li><a href="#">Kelly Goto</a></li>
+      <li><a href="#">Kelly Goto</a></li>
+      <li><a href="#">Kelly Goto</a></li>
+    </ul>
+  </div>
+
 
   </div>
   <div class="home-ctas">
-    <div class="button button-home">
-      <a href="schedule">See the full line-up</a>
-    </div> 
     <div class="sponsors">
     	<h6>Signature sponsor</h6>
       <a href="http://ebay.com"><img src="http://cdn.mindtheproduct.com/conference/2013/assets/images/logos/ebay.png" alt="Ebay" /></a>
