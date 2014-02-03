c96f0015b35fa74f294f02aa521762fe2f9cea36
Martin Eriksson <martin@mindtheproduct.com>
2014-02-03 13:36:05 -0500

    Added coming soon date

--- index.php
+++ index.php
@@ -9,23 +9,19 @@
     <h3>Watch the videos and relive the magic.</h3>
   
     <div class="columns"
-    <p>We hope you enjoyed this year’s sessions.  Missed it?  You can watch the videos, read reviews and more from the event on MindTheProduct’s blog.</p>
+    <p>We hope you enjoyed this year’s conference.  If you missed it you can watch the videos, read reviews and more on <a href="http://www.mindtheproduct.com" title="Product Management Blog">Mind the Product</a>.</p>
 
     <ul>
-      <li><a href="http://www.mindtheproduct.com/2013/10/mind-the-product-2013-conference-recap/">Recap Video</a></li>
-      <li><a href="http://www.mindtheproduct.com/2013/10/mind-product-2013-create-amazing-products-exit-billion-dollars-change-future-2/">Recap blog post</a></li>
-
-      <!--
-      <li><a href="#">Kelly Goto</a></li>
-      <li><a href="#">Kelly Goto</a></li>
-      <li><a href="#">Kelly Goto</a></li>
-      <li><a href="#">Kelly Goto</a></li>
-      <li><a href="#">Kelly Goto</a></li>
-      <li><a href="#">Kelly Goto</a></li>
-      <li><a href="#">Kelly Goto</a></li>
-    -->
+      <li><a href="http://www.mindtheproduct.com/2013/10/mind-the-product-2013-conference-recap/" title="Product Management Conference">The Conference Video</a></li>
+      <li><a href="http://www.mindtheproduct.com/2013/10/mind-product-2013-create-amazing-products-exit-billion-dollars-change-future-2/" title="Product Management Conference">Mind the Product 2013: How to Create Amazing Products, Exit for a Billion Dollars and Change the Future</a></li>
+      <li><a href="http://www.mindtheproduct.com/tag/mind-the-product-2013/" title="Product Management Conference Video">Watch all the talks</a>
     </ul>
   </div>
+
+  <br /><br />
+
+  <h3>Mind the Product will be back on September 12, 2014</h3>
+  <p> Watch this space for more details on an even bigger and better product management conference than ever!</p>
 
 
   </div>
