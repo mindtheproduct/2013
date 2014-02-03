1226b4fe1171c5f74fffb106fd10f81db63533dc
Martin Eriksson <martin@mindtheproduct.com>
2014-02-03 13:47:40 -0500

    Update

--- index.php
+++ index.php
@@ -8,23 +8,24 @@
   <div class="content-home">
     <h3>Watch the videos and relive the magic.</h3>
   
-    <div class="columns"
-    <p>We hope you enjoyed this year’s conference.  If you missed it you can watch the videos, read reviews and more on <a href="http://www.mindtheproduct.com" title="Product Management Blog">Mind the Product</a>.</p>
+    <div class="columns">
+    <p>We hope you enjoyed this year’s conference.  If you missed it you can watch the videos, read reviews and more on on our blog:</p>
 
     <ul>
       <li><a href="http://www.mindtheproduct.com/2013/10/mind-the-product-2013-conference-recap/" title="Product Management Conference">The Conference Video</a></li>
-      <li><a href="http://www.mindtheproduct.com/2013/10/mind-product-2013-create-amazing-products-exit-billion-dollars-change-future-2/" title="Product Management Conference">Mind the Product 2013: How to Create Amazing Products, Exit for a Billion Dollars and Change the Future</a></li>
-      <li><a href="http://www.mindtheproduct.com/tag/mind-the-product-2013/" title="Product Management Conference Video">Watch all the talks</a>
+      <li><a href="http://www.mindtheproduct.com/2013/10/mind-product-2013-create-amazing-products-exit-billion-dollars-change-future-2/" title="Product Management Conference">Conference Recap</a></li>
+      <li><a href="http://www.mindtheproduct.com/tag/mind-the-product-2013/" title="Product Management Conference Video">Watch all the talks</a></li>
     </ul>
-  </div>
+    </div>
 
-  <br /><br />
 
   <h3>Mind the Product will be back on September 12, 2014</h3>
-  <p> Watch this space for more details on an even bigger and better product management conference than ever!</p>
+  <p>Watch this space for more details on our 2014 conference, it'll be even bigger and better!</p>
 
 
   </div>
+
+  
   <div class="home-ctas">
     <div class="sponsors">
     	<h6>Signature sponsor</h6>
